29d92749573c322de4f776fc85eca9374a0c3e67
ducit95 <ducit95@users.noreply.github.com>
2016-06-18 11:28:43 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,1 @@
-var app = angular.module('App', [])
-    .constant('API_URL', 'http://test-app.dev/');
 
-app.directive('fileModel', ['$parse', function($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
-
-            element.bind('change', function() {
-                scope.$apply(function() {
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
-    };
-}]);
