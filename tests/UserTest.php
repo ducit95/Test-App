ba29a6bbdb5b0fb2ce2606fbb154ed3179ef04f8
ducit95 <ducit95@users.noreply.github.com>
2016-06-21 15:18:16 +0700

    Update UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -12,39 +12,96 @@ class UserTest extends TestCase
      * @return void
      */
     public function testExample()
+      {
+        $this->assertTrue(true);
+      }
+    public function testIndex()
     {
         $response = $this->call('GET', '/');
-
-    	$this->assertEquals(200, $response->status());
+        $this->see('LIst User');
+        $this->assertEquals(200, $response->status());
+        $this->assertResponseOk();
     }
-     public function testGedata()
-    {
-        $response = $this->call('GET', 'getdata');
-
-    	$this->assertEquals(200, $response->status());
+    public function testCreateUSer(){
+      $response = $this->call('POST', 'create',['name' => 'Taylor','address' =>'HA NOI','age'=>6]);
+      $this->assertResponseOk();
+      $this->seeInDatabase('test_app_user', ['name' => 'Taylor','name' => 'Taylor','address' =>'HA NOI','age'=>6]);
     }
-    public function testDetele()
-    {
-        $response = $this->call('GET', 'detele/10');
-
-    	$this->assertEquals(200, $response->status());
+    public function testVali_name_User_CreateUSer(){
+      $response = $this->call('POST', 'create',['name' => '','address' =>'kaka','age'=>6]);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['address' => 'kaka']);
     }
-    public function testDatauser()
-    {
-        $response = $this->call('GET', 'getdatauser/11');
-
-    	$this->assertEquals(200, $response->status());
+    public function testVali_address_CreateUSer(){
+      $response = $this->call('POST', 'create',['name' => 'kaka','address' =>'','age'=>6]);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['name' => 'kaka']);
     }
-    public function testUpdate()
-    {
-        $response = $this->call('POST', 'update/11',['name' => 'Taylor','address' =>'HA NOI','age'=>9]);
-
-    	$this->assertEquals(200, $response->status());
+    public function testVali_age_CreateUSer(){
+      $response = $this->call('POST', 'create',['name' => 'kaka','address' =>'HA NOI','age'=>'']);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['name' => 'kaka']);
     }
-    public function testCreate()
-    {
-        $response = $this->call('POST', 'create',['name' => 'Taylor','address' =>'HA NOI','age'=>6]);
-
-    	$this->assertEquals(200, $response->status());
+    public function testVali_name_address_CreateUSer(){
+      $response = $this->call('POST', 'create',['name' => '','address' =>'','age'=>99]);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['age' => '99']);
+    }
+    public function testVali_address_age_CreateUSer(){
+      $response = $this->call('POST', 'create',['name' => 'kaka','address' =>'','age'=>'']);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['name' => 'kaka']);
+    }
+    public function testVali_name_age_CreateUSer(){
+      $response = $this->call('POST', 'create',['name' => '','address' =>'kaka','age'=>'']);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['address' => 'kaka']);
+    }
+    public function testVali_all_CreateUSer(){
+      $response = $this->call('POST', 'create',['name' => '','address' =>'','age'=>'']);
+       $this->assertFalse(false);
+    }
+     public function testEdit(){
+      $response = $this->call('POST', 'update/92',['name' => 'Taylor92','address' =>'HA NOI','age'=>6]);
+      $this->assertResponseOk();
+      $this->seeInDatabase('test_app_user', ['id' => 92,'name' => 'Taylor92','address' =>'HA NOI','age'=>6]);
+    }
+    public function testVali_name_User_Edit(){
+      $response = $this->call('POST', 'update/92',['name' => '','address' =>'kaka','age'=>6]);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['id'=>92,'address' => 'kaka']);
+    }
+    public function testVali_address_Edit(){
+      $response = $this->call('POST', 'update/92',['name' => 'kaka','address' =>'','age'=>6]);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['id'=>92,'name' => 'kaka']);
+    }
+    public function testVali_age_Edit(){
+      $response = $this->call('POST', 'update/92',['name' => 'kaka','address' =>'HA NOI','age'=>'']);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['id'=>92,'name' => 'kaka']);
+    }
+    public function testVali_name_address_Edit(){
+      $response = $this->call('POST', 'update/92',['name' => '','address' =>'','age'=>99]);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['id'=>92,'age' => '99']);
+    }
+    public function testVali_address_age_Edit(){
+      $response = $this->call('POST', 'update/92',['name' => 'kaka','address' =>'','age'=>'']);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['id'=>92,'name' => 'kaka']);
+    }
+    public function testVali_name_age_Edit(){
+      $response = $this->call('POST', 'update/92',['name' => '','address' =>'kaka','age'=>'']);
+      $this->assertResponseOk();
+      $this->notSeeInDatabase('test_app_user', ['id'=>92,'address' => 'kaka']);
+    }
+    public function testVali_all_Edit(){
+      $response = $this->call('POST', 'update/92',['id'=>92,'name' => '','address' =>'','age'=>'']);
+       $this->assertFalse(false);
+    }
+    public function testDetete(){
+      $response = $this->call('GET', 'detele/96',[]);
+      $this->notSeeInDatabase('test_app_user', ['id'=>96]);
     }
 }
